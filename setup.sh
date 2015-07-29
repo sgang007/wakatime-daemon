dbf07ed0ef4ddab976fe034194c59f7dd928aa18
Shubhojyoti <gangulyshubhojyoti@gmail.com>
2015-07-29 13:35:04 +0530

    Setup for installing wakatime icons is added

---
+++ setup.sh
@@ -0,0 +1,11 @@
+cp ./resources/icons/wakatime-notification-160.png /usr/share/icons/wakatime.png
+cp ./resources/icons/wakatime-logo-128.png /usr/share/icons/hicolor/128x128/apps/wakatime.png
+cp ./resources/icons/wakatime-logo-16.png /usr/share/icons/hicolor/16x16/apps/wakatime.png
+cp ./resources/icons/wakatime-logo-19.png /usr/share/icons/hicolor/20x20/apps/wakatime.png
+cp ./resources/icons/wakatime-logo-48.png /usr/share/icons/hicolor/48x48/apps/wakatime.png
+cp ./resources/icons/wakatime-logo-48-red.png /usr/share/icons/hicolor/48x48/apps/wakatime-red.png
+cp ./resources/icons/wakatime-logo-48-white.png /usr/share/icons/hicolor/48x48/apps/wakatime-white.png
+cp ./resources/icons/wakatime-logo-38.png /usr/share/icons/hicolor/40x40/apps/wakatime.png
+cp ./resources/icons/wakatime-logo-38-red.png /usr/share/icons/hicolor/40x40/apps/wakatime-red.png
+cp ./resources/icons/wakatime-logo-38-white.png /usr/share/icons/hicolor/40x40/apps/wakatime-white.png
+cp ./resources/icons/wakatime-logo-38-gray.png /usr/share/icons/hicolor/40x40/apps/wakatime-gray.png
